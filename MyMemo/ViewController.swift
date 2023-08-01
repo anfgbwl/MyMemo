042d488095c6f5a4012bd7cc1db091c75637d7f8
Seoon KIM <6872065@gmail.com>
2023-08-01 17:30:53 +0900

    Lv1. Todo 홈 화면 만들기 (TodoViewController)

--- MyMemo/ViewController.swift
+++ MyMemo/ViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
     @IBAction func firstButtonTouch(_ sender: UIButton) {
         print("버튼 클릭 : 할 일 확인하기")
+//        self.firstButton.backgroundColor = .white
+//        self.firstButton.tintColor = .blue
+        // (목표) 버튼 눌렀을 때 배경, 폰트 색상 바꾸기
     }
     @IBAction func secondButtonTouch(_ sender: UIButton) {
         print("버튼 클릭 : 완료한 일 보기")
