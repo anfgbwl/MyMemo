141765c7a8dba01883aaad6c6df160773bee8d3f
Seoon KIM <6872065@gmail.com>
2023-08-03 17:16:47 +0900

    Lv2 메모 추가 및 데이터로드 구현

--- MyMemo/ComposeViewController.swift
+++ MyMemo/ComposeViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class ComposeViewController: UIViewController {
+    // 멤버 변수로 memo 선언
+    var memo: String?
+    
+    @IBOutlet weak var memoTextView: UITextView!
     
     @IBAction func close(_ sender: Any) {
+        //창 사라짐
         dismiss(animated: true, completion: nil)
     }
-    
+    @IBAction func save(_ sender: Any) {
+        if let memo = memoTextView.text,
+           memo.count > 0 {
+            // save 함수의 지역 변수 memo 값을 멤버 변수 memo에 할당
+            self.memo = memo
+            
+            // 여기 Alert는 확인누르면 메모리스트에 추가, 취소는 저장안하기
+            // 타이틀, 메세지만 설정하고 확인/취소는 UIViewController+Alert 파일에서 설정한 행위 이행
+            alert(title: "저장하기", message: "저장하시겠습니까?")
+            
+            
+        } else {
+            // 여기 Alert는 확인만 뜨게 하기
+            alert(title: "저장하기", message: "내용이 없습니다.")
+            return
+        }
+    }
     
 
     override func viewDidLoad() {
--- MyMemo/TableViewController.swift
+++ MyMemo/TableViewController.swift
@@ -8,74 +8,97 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    // 나중에 시간 표시할 수도 있으니까 남겨두기
     let formatter: DateFormatter = {
         let f = DateFormatter()
         f.dateStyle = .long
         f.timeStyle = .short
         return f
     }()
-    
-    var memo = [Memo]()
-    
-    @IBAction func addList(_ sender: Any) {
-        // sender를 UIButton으로 하니까 안떴음... Any로 해야 뜨네..?
-        print("버튼 클릭 : 추가")
-        let title = "할 일 추가"
-        let message = ""
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let cancel = UIAlertAction(title: "취소", style: .default)
-        let ok = UIAlertAction(title: "추가", style: .default) { (_) in
-            // 확인 클릭 시 할 일 목록에 추가하는 코드 작성
-        }
-        alert.addAction(cancel)
-        alert.addAction(ok)
         
-        alert.addTextField() {(tf) in
-            tf.placeholder = "할 일을 입력해주세요."
+    @IBAction func addList(_ sender: UIBarButtonItem) {
+        print("버튼 클릭 : 추가")
+        
+        // 인스턴스 생성
+        let alert = UIAlertController(title: "할 일 추가", message: "", preferredStyle: .alert) // preferredStyle: .actionSheet -> 텍스트필드 없고 목록만 있는 형태
+        
+        // Alert textField 작성 가이드
+        alert.addTextField() { (tf) in
+            tf.placeholder = "내용을 입력하세요."
         }
-        self.present(alert, animated: true)
+        
+        // 확인 버튼 객체 생성
+        let ok = UIAlertAction(title: "확인", style: .default) { (_) in // 트레일링 클로저
+            // 내용을 입력했을 때
+            if let memoTitle = alert.textFields?[0].text {
+                // 입력값을 이용하여 Memo 객체 생성
+                let newMemo = Memo(content: memoTitle)
+                // Memo 객체를 MemoList 배열에 추가
+                Memo.MemoList.append(newMemo)
+                self.tableView.reloadData()
+            }
+        }
+        
+        // 취소 버튼 객체 생성
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        // Alert에 버튼 객체 등록
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        // Alert 띄우기
+        self.present(alert, animated: false)
+
     }
     
-    //    var strikeThrough = false
-//    @IBAction func listSwitch(_ sender: UISwitch) {
-//        strikeThrough = sender.isOn
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
 //        tableView.reloadData()
-//    }
+//        print(#function)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
+        
+        tableView.reloadData()
+   }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Memo.dummyMemoList.count
+        return Memo.MemoList.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
-            
-        let target = Memo.dummyMemoList[indexPath.row]
-//        cell.textLabel?.text = nil // 하 어이없어 전에 써논거 남아서 겹쳐나옴 ㅋㅋㅋㅋㅋㅋ
-        cell.textLabel?.text = target.content
-        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
-        // 메모 작성날짜 삽입
         
-//        let labelAttributedString = NSMutableAttributedString(string: "할 일 목록")
-//        if strikeThrough {
-//            labelAttributedString.addAttribute(.strikethroughStyle, value: 1, range: NSRange(location: 0, length: labelAttributedString.length))
-//        } else {
-//            labelAttributedString.removeAttribute(.strikethroughStyle, range: NSRange(location: 0, length: labelAttributedString.length))
-//        }
-//        cell.toDoListLabel.attributedText = labelAttributedString
-
+        // TableViewCell 클래스에서 정의한 셀로 적용
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        
+        // 셀이 사용될 때마다 라벨/스위치 초기화
+//        cell.memoLabel.text = nil
+            
+        // 셀의 기본 텍스트 레이블 행 수 제한 없음
+//        cell.textLabel?.numberOfLines = 1
+        
+        // 셀의 기본 텍스트 레이블에 배열 변수의 값을 할당
+        // >> 이게 잘못됐네.. 셀의 기본 텍스트 레이블이 아니라 커스텀 셀안에 있는 memoLabel에 넣어야 함...
+        let target = Memo.MemoList[indexPath.row]
+        cell.memoLabel?.text = target.content
+                
+        // 메모 작성날짜 삽입
+//        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
         return cell
     }
 
 }
 
 class TableViewCell: UITableViewCell {
-    
-//    @IBOutlet weak var toDoListLabel: UILabel!
-//    @IBOutlet weak var listSwitch: UISwitch!
+    @IBOutlet weak var memoLabel: UILabel!
+    @IBOutlet weak var memoSwitch: UISwitch!
+    @IBAction func memoSwitch(_ sender: UISwitch) {
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -84,6 +107,21 @@ class TableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
+    
+    // 셀의 재사용
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        memoLabel.text = nil
+    }
+    
+//    // 라벨의 크기를 설정하는 메서드 (오버라이드)
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//
+//        // 라벨의 크기를 셀의 컨텐츠 뷰의 크기에 맞게 설정
+//        memoLabel.frame = memoLabel.bounds
+//    }
     
 }
 
