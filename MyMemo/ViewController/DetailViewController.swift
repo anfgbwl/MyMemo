5321ee4a333326386af0f9cd7bf8474f74e1f545
Seoon KIM <6872065@gmail.com>
2023-08-11 12:58:56 +0900

    폴더 구조 정리 및 업데이트 기능 구현 1. 목표일자 저장 2. 중요도 및 카테고리 저장(state 체크 미설정, 조치 필요) 3. 진행율 20% 간격으로 선택 설정

--- MyMemo/ViewController/DetailViewController.swift
+++ MyMemo/ViewController/DetailViewController.swift
@@ -15,7 +15,6 @@ class DetailViewController: UIViewController {
     var prepareMemo: Memo?
     var prepareMemoIndex: Int?
     
-    
     @IBOutlet weak var memoTextView: UITextView!
     @IBOutlet weak var targetDatePicker: UIDatePicker!
     @IBOutlet weak var priorityButton: UIButton!
@@ -24,10 +23,11 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var memoManagement: UIBarButtonItem!
     
     @IBAction func progressSlider(_ sender: UISlider) {
+        sender.value = roundf(sender.value)
         let progressValue = sender.value
         prepareMemo?.progress = Int(progressValue)
     }
-
+    
     @IBAction func save(_ sender: Any) {
         let content = memoTextView.text ?? ""
         let isCompleted = true
@@ -58,6 +58,31 @@ class DetailViewController: UIViewController {
         self.present(memoManagementAlert, animated: true)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 여기에 setTitle 적용하면 디테일페이지 버튼에 디폴트 값이 안뜸, 그리고 디폴트 값 선택해도 버튼에 안뜸(저장은 잘됨, 다시 로드해도 잘 뜸) -> 일단 저장된 데이터는 불러와야하니까 얘로 설정
+//        priorityButton.setTitle(prepareMemo?.priority ?? "없음", for: .normal)
+//        categoryButton.setTitle(prepareMemo?.category ?? "일반", for: .normal)
+        
+        // 우와 해결!!!!! 빈값도 nil이 아니라 값으로 인지하고 있어서 조건문으로 설정
+        // 근데 문제는 선택된 항목에 체크가 안되고 디폴트에 되어 있음
+        if prepareMemo?.priority == "", prepareMemo?.category == "" {
+            priorityButton.setTitle("없음", for: .normal)
+            categoryButton.setTitle("일반", for: .normal)
+        } else {
+            priorityButton.setTitle(prepareMemo?.priority, for: .normal)
+            priorityButton.titleLabel?.text = prepareMemo?.priority
+            categoryButton.setTitle(prepareMemo?.category, for: .normal)
+            categoryButton.titleLabel?.text = prepareMemo?.category
+        }
+        
+        // nil 이 아닐때만 표시(실패)
+//        if let priority = prepareMemo?.priority, let category = prepareMemo?.category {
+//            priorityButton.setTitle(priority, for: .normal)
+//            categoryButton.setTitle(category, for: .normal)
+//        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -65,25 +90,47 @@ class DetailViewController: UIViewController {
         memoTextView.isScrollEnabled = false
         memoTextView.text = prepareMemo?.content
         targetDatePicker.date = prepareMemo?.targetDate ?? Date()
-        setpriorityButton()
+        
+        // 버튼 상태변경(데이터 저장은 되나 불러올 때 기본값으로만 불러와지는 문제 발생)
+        
+        // 1. setTitle 설정 (실패) -> priority != priorityButton
+//        priorityButton.setTitle(prepareMemo?.priority, for: .normal)
+//        categoryButton.setTitle(prepareMemo?.category, for: .normal)
+        
+        // 2. 옵셔널 바인딩 시도 (실패) -> priority != priorityButton
+        //        if let priority = prepareMemo?.priority {
+        //            priorityButton.setTitle(priority, for: .normal)
+        //        }
+        //        if let category = prepareMemo?.category {
+        //            categoryButton.setTitle(category, for: .normal)
+        //        }
+        
+        // 3. titleLabel.text 설정 (실패) -> priority == priorityButton, 시뮬레이터에는 적용안됨(타이틀 설정이 안됐는데, 타이틀 텍스트는 적용됨; 무슨일?)
+//        priorityButton.titleLabel?.text = prepareMemo?.priority
+//        categoryButton.titleLabel?.text = prepareMemo?.category
+        
+        progressSlider.value = Float(prepareMemo?.progress ?? 0)
+        setPriorityButton()
         setCategoryButton()
         progressSlider.value = Float(prepareMemo?.progress ?? 0)
     }
     
-    func setpriorityButton() {
+    
+    func setPriorityButton() {
         let seletedPriority = {(action: UIAction) in
-            print(action.title)}
+//             여기서 지정해줘야 하나?(실패)
+//            self.priorityButton.setTitle(action.title, for: .normal)
+        }
         priorityButton.menu = UIMenu(children: [
-            UIAction(title: "없음", state: .on, handler: seletedPriority),
+            UIAction(title: "없음", handler: seletedPriority),
             UIAction(title: "낮음", handler: seletedPriority),
             UIAction(title: "중간", handler: seletedPriority),
-            UIAction(title: "높음", handler: seletedPriority)])
+            UIAction(title: "높음", attributes: .destructive, handler: seletedPriority)])
         priorityButton.showsMenuAsPrimaryAction = true
         priorityButton.changesSelectionAsPrimaryAction = true
     }
     
     func setCategoryButton() {
-
         let normal = UIAction(title: "일반", handler: { _ in print("일반") })
         let pet = UIAction(title: "반려동물", handler: { _ in print("반려동물") })
         let home = UIAction(title: "집", handler: { _ in print("집") })
@@ -94,9 +141,13 @@ class DetailViewController: UIViewController {
         self.categoryButton.changesSelectionAsPrimaryAction = true
     }
     
+    // Slider thumb 사이즈 조절 필요(진행 중, 미완성)
+    //    func setProgressSlider() {
+    ////        progressSlider.setThumbImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControl.State#>)
+    //        let thumbsize = CGSize(width: 10, height: 10)
+    //    }
+    
 }
-
-
 
 extension DetailViewController : UITextViewDelegate {
     
